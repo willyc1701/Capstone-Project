2dabb7c6e0d96d07cca600638989b6ae4c60a755
willyc1701 <88260789+willyc1701@users.noreply.github.com>
2023-10-11 17:10:51 +0800

    Updated Device.cs

--- DeviceRecordLoan/DeviceRecordLoan/Device.cs
+++ DeviceRecordLoan/DeviceRecordLoan/Device.cs
@@ -8,5 +8,21 @@ namespace DeviceRecordLoan
 {
     internal class Device
     {
+        public string ModelName { get; private set; }
+        public string ModelID { get; private set; }
+        public int YearBought { get; private set; }
+        public decimal Cost { get; private set; }
+        public int Quantity { get; private set; }
+        public List<int> Status { get; private set; }
+
+        public Device(string modelName, string modelID, int yearBought, decimal cost, int quantity)
+        {
+            ModelName = modelName;
+            ModelID = modelID;
+            YearBought = yearBought;
+            Cost = cost;
+            Quantity = quantity;
+            Status = new List<int>(quantity);
+        }
     }
 }
